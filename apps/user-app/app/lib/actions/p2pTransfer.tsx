e3daa38e980ea92d8d9b525c3cf599bbfabd0b54
mohit32886 <mohit32886@gmail.com>
2024-04-04 09:48:26 +0530

    correcting type errors

--- apps/user-app/app/lib/actions/p2pTransfer.tsx
+++ apps/user-app/app/lib/actions/p2pTransfer.tsx
@@ -3,6 +3,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../auth";
 import prisma from "@repo/db/client";
 
+
 export async function p2pTransfer(to: string, amount: number) {
     const session = await getServerSession(authOptions);
     const from = session?.user?.id;
@@ -22,7 +23,7 @@ export async function p2pTransfer(to: string, amount: number) {
             message: "User not found"
         }
     }
-    await prisma.$transaction(async (tx) => {
+    await prisma.$transaction(async (tx:any) => {
         await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
 
         const fromBalance = await tx.balance.findUnique({
